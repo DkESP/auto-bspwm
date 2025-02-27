caa4b5c5189b757ebbf88eace37211375a5882ad
DkESP <143833898+DkESP@users.noreply.github.com>
2025-02-27 22:39:53 +0100

    Update target-to-hack.sh

--- config/bspwm/scripts/target-to-hack.sh
+++ config/bspwm/scripts/target-to-hack.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
- 
-ip_address=$(/bin/cat /home/user/.config/bin/target | awk '{print $1}')
-machine_name=$(/bin/cat /home/user/.config/bin/target | awk '{print $2}')
- 
-if [ $ip_address ] && [ $machine_name ]; then
-    echo "%{F#e51d0b}󱩊 $ip_address%{u-} - $machine_name"
+
+user="$(whoami)"
+target_file="/home/${user}/.config/bin/target"
+
+# Verificar si el archivo existe
+if [ -f "$target_file" ]; then
+    ip_address=$(awk '{print $1}' "$target_file")
+    machine_name=$(awk '{print $2}' "$target_file")
+
+    if [ -n "$ip_address" ] && [ -n "$machine_name" ]; then
+        echo "%{F#e51d0b}󱩊 $ip_address%{u-} - $machine_name"
+    else
+        echo "%{F#e51d0b}󱩊 %{u-} No target"
+    fi
 else
-    echo "%{F#e51d0b}󱩊 %{u-} No target"
+    echo "%{F#e51d0b}󱩊 %{u-} No target file"
 fi
